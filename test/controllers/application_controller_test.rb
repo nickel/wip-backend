2ce6ef13ce48f1e79c8674ced5359d38582a560d
Juan Gallego IV <juan@gallegoiv.com>
2024-05-01 20:51:58 +0200

    refactoring tests

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -13,40 +13,43 @@ class ApplicationControllerTest < ActionController::TestCase
   tests DummyController
 
   test "should authorize a request with a valid token" do
-    with_routing do |set|
-      set.draw do
-        resources :dummy, controller: "application_controller_test/dummy"
-      end
+    with_dummy_routing do
+      token = Iam::Session::Create
+        .call(username: "test", password: "test")
+        .value[:token]
 
-      begin
-        token = Iam::Session::Create
-          .call(username: "test", password: "test")
-          .value[:token]
+      @request.headers["Authorization"] = "Bearer #{token}"
 
-        @request.headers["Authorization"] = "Bearer #{token}"
-
-        get :index
-        assert_response :success
-      end
+      get :index
+      assert_response :success
     end
   end
 
   test "should NOT authorize a request with an invalid token" do
+    with_dummy_routing do
+      @request.headers["Authorization"] = "Bearer invalid"
+
+      get :index
+      assert_response :unauthorized
+    end
+  end
+
+  test "should NOT authorize a request with no token" do
+    with_dummy_routing do
+      get :index
+      assert_response :unauthorized
+    end
+  end
+
+  private
+
+  def with_dummy_routing
     with_routing do |set|
       set.draw do
         resources :dummy, controller: "application_controller_test/dummy"
       end
 
-      begin
-        token = Iam::Session::Create
-          .call(username: "test", password: "test")
-          .value[:token]
-
-        @request.headers["Authorization"] = "Bearer invalid"
-
-        get :index
-        assert_response :unauthorized
-      end
+      yield
     end
   end
 end
